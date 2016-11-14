479ad3b00c305b98e709de6f96a01b6e9297a6ad
pxai <pello_altadill@cuatrovientos.org>
2016-11-15 00:29:59 +0100

    Added some changes in UI

--- app/src/main/java/io/pello/android/notificationsample/MainActivity.java
+++ app/src/main/java/io/pello/android/notificationsample/MainActivity.java
@@ -9,34 +9,41 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
+import android.view.View;
 
+/**
+ * Shows the use of notifications
+ * @author Pello Altadill
+ * Check documentation at:
+ * https://developer.android.com/guide/topics/ui/notifiers/notifications.html
+ */
 public class MainActivity extends AppCompatActivity {
-
+    private int notifyId = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void notifySomething (View v) {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-            mBuilder.setSmallIcon(R.drawable.notify);
-            mBuilder.setContentTitle("My notification");
-            mBuilder.setContentText("Hello World!");
-//                new NotificationCompat.Builder(this)
-//                        .setSmallIcon(R.drawable.notify)
-//                        .setContentTitle("My notification")
-//                        .setContentText("Hello World!");
-// Creates an explicit intent for an Activity in your app
+        mBuilder.setSmallIcon(R.drawable.notify);
+        mBuilder.setContentTitle("New notification by Sample App");
+        mBuilder.setContentText("Somebody has a message for you");
+
+        // This will be called from the notification
         Intent resultIntent = new Intent(this, CalledFromNotification.class);
 
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
+        // We create a stack for the new Activity
+        // when we enter the notification and click back we will appear in the main.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
+
+       // // Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(CalledFromNotification.class);
-// Adds the Intent that starts the Activity to the top of the stack
+
+        // Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent);
+
         PendingIntent resultPendingIntent =
                 stackBuilder.getPendingIntent(
                         0,
@@ -45,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
         mBuilder.setContentIntent(resultPendingIntent);
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        mNotificationManager.notify(1, mBuilder.build());
+
+        // notifyId allows us to update the notification, changing the number
+        mNotificationManager.notify(notifyId, mBuilder.build());
     }
 }
